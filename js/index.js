61a34173b395c2aa8f90ff6beaeddeec3442d139
malfinhanafi <finmarhas@gmail.com>
2025-08-06 17:43:28 +0700

    alsa

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'KA SALSAAA'.split('')
+const text = 'ALSAAA'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('GOOD NIGHT KA SALSAA').split('')
+    const titles = ('JANGAN LUPA SENYUM ALSAA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
